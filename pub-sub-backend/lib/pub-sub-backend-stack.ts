19c8239eb0f93a5f649d7d600ddb50deafa0d537
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2021-05-27 04:50:14 +0500

    I have create mapping templates which are written in Apache VTL.

--- pub-sub-backend/lib/pub-sub-backend-stack.ts
+++ pub-sub-backend/lib/pub-sub-backend-stack.ts
@@ -7,6 +7,7 @@ import * as lambda from '@aws-cdk/aws-lambda';
 import * as sns from '@aws-cdk/aws-sns';
 import * as sqs from '@aws-cdk/aws-sqs';
 import * as subscriptions from '@aws-cdk/aws-sns-subscriptions';
+import { responseTemplate, requestTemplate, EVENT_SOURCE } from '../utils/appsync-request-response';
 
 export class PubSubBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -38,8 +39,7 @@ export class PubSubBackendStack extends cdk.Stack {
     const PetTheoryDS = petTheoryApi.addDynamoDbDataSource('pet-theoryTableEvent', PetTheoryTable);
 
     // HTTP as Datasource for the Graphql API
-    //// Create Http Data source that will put our event to the eventbus
-    
+    //// Create Http Data source that will put our event to the eventbus    
     const httpEventTriggerDS = petTheoryApi.addHttpDataSource(
       "eventTriggerDS",
       "https://events." + this.region + ".amazonaws.com/", // This is the ENDPOINT for eventbridge.
@@ -54,6 +54,35 @@ export class PubSubBackendStack extends cdk.Stack {
     );
 
     events.EventBus.grantAllPutEvents(httpEventTriggerDS);
+
+    ///////////////  APPSYNC  Resolvers   ///////////////
+
+    /* Query */
+    PetTheoryDS.createResolver({
+      typeName: "Query",
+      fieldName: "getReports",
+      requestMappingTemplate: appsync.MappingTemplate.dynamoDbScanTable(),
+      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultList(),
+    });
+
+    /* Mutation */
+    const mutations = ["addReport", "deleteReport"]
+    mutations.forEach((mut) => {
+      let details = `\\\"todoId\\\": \\\"$ctx.args.todoId\\\"`;
+      if (mut === 'addReport') {
+        details = `\\\"firstName\\\":\\\"$ctx.args.report.firstName\\\" , \\\"lastName\\\":\\\"$ctx.args.report.lastName\\\" , \\\"reportTitle\\\":\\\"$ctx.args.report.reportTitle\\\" , \\\"desc\\\":\\\"$ctx.args.report.desc\\\"`
+      } else if (mut === "deleteReport") {
+        details = `\\\"reportId\\\":\\\"$ctx.args.reportId\\\"`
+      }
+
+      httpEventTriggerDS.createResolver({
+        typeName: "Mutation",
+        fieldName: mut,
+        requestMappingTemplate: appsync.MappingTemplate.fromString(requestTemplate(details, mut)),
+        responseMappingTemplate: appsync.MappingTemplate.fromString(responseTemplate()),
+      });
+    });
+
 
     
 
